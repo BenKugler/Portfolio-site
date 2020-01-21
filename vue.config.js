0150015055c17c427ad29f8e41fb525d8dddb7d9
Ben Kugler <benjamin.kugler7@gmail.com>
2020-01-20 19:49:19 -0800

    Corrected vue build

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
-import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin';
-
 export default {
   "transpileDependencies": [
     "vuetify"
   ],
-  configureWebpack: {
-    plugins: [
-      new VuetifyLoaderPlugin()
-    ],
-  },
 }
